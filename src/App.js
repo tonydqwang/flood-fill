53ca7447b01c1cd6bb15e0ba34f5e244f1eb4265
Tony Wang <tonydqwang@gmail.com>
2021-05-09 15:04:03 -0400

    add blank canvas and basic styling

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import 'devextreme/dist/css/dx.common.css';
 import 'devextreme/dist/css/dx.light.css';
 import './App.css';
 import Header from './components/header';
+import Canvas from './components/canvas';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
@@ -19,6 +20,21 @@ function App() {
     [prefersDarkMode],
   );
 
+  const genData = (sizeX, sizeY) => {
+    const data = Array(sizeX, sizeY)
+    for (var y = 0; y < sizeY; y++) {
+      for (var x = 0; x < sizeX; x++) {
+        data[y * sizeX + x] = { x, y, color: '#ffffff' };
+      }
+    }
+    return data;
+  }
+
+  const canvasSizeX = 5;
+  const canvasSizeY = 5;
+  const activeColor = '#ffffff';
+  const canvasData = genData(canvasSizeX, canvasSizeY);
+
   return (
     <>
       <ThemeProvider theme={theme}>
@@ -26,6 +42,12 @@ function App() {
         <Header />
         <main>
           <div className='main'>
+            <Canvas 
+              data={canvasData} 
+              sizeX={canvasSizeX} 
+              sizeY={canvasSizeY}
+              color={activeColor}
+            />
           </div>
         </main>
       </ThemeProvider>
